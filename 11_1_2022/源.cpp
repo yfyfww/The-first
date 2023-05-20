c1c584d3a8e31e7e4e3eec6dcfdbea132dad4855
yfyfww <134151321+yfyfww@users.noreply.github.com>
2023-05-20 23:08:38 +0800

    Add files via upload

---
+++ 11_1_2022/源.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+struct crad {
+	char name[20] ;
+	int price ;
+};
+int main()
+{
+	int i=0,a = 1, b = 0, c = 3;
+	i = a++ && b++ && ++c;
+	printf("a=%d b=%d c=%d\n", a, b, c);
+	b = (a > 2 ? 6 : 9);
+	printf("a=%d b=%d c=%d\n", a, b, c);
+	i = (c = 6, a = c + 2, b = a + c, c += 6);
+	printf("i=%d\n", i);
+	if (c = 7, a =  2,i=2, c == 7);
+	{
+		printf("a=%d\n", a);
+	}
+	printf("i=%d\n", i);
+	struct crad m = { "詹姆斯签名球星卡",10 };
+	printf("卡名 ：%s\n",m.name );
+	printf("价格 ： % d\n", m.price);
+
+	char z = 8;//00000000 00000000 00000000 00000100,而字符类型z只能存入一个字节，所以z=00001000
+	char x = 127;//x=01111111
+	char v = z + x;//有符号数补位取决于符号位，无符号数补位为零 整形提升， v=10000111
+	printf("v=%d\n", v);//输出结果为整形打印，v进行补位，v=11111111 11111111 11111111 10000111 -计算机以补码形式存储
+	                    //原码为10000000 00000000 00000000 01111001—— -121
+	char n = 0xef;
+	printf("n=%d\n", n);
+	return 0;
+}
